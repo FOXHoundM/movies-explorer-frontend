ba2b360274a0322425765eafb208e6ba26d2c488
FOXHoundM <4216623@gmail.com>
2023-02-09 15:43:15 +0300

    pull request done

--- src/components/Movies/MoviesCard/MoviesCard.jsx
+++ src/components/Movies/MoviesCard/MoviesCard.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import './MoviesCard.css';
-import {useState} from 'react';
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
-const MoviesCard = ({trailerLink, imageLink, duration, nameRU}) => {
-
+const MoviesCard = ({ trailerLink, imageLink, duration, nameRU }) => {
+	const location = useLocation();
 	const hours = Math.floor(duration / 60);
 	const minutes = Math.floor(duration - hours * 60);
 	const [isFavorite, setIsFavorite] = useState(false);
 
 	const cardLikeButtonClassName = `movies-card__favorite ${
-		isFavorite
-			? 'movies-card__favorite_active'
-			: ''
+		isFavorite ? 'movies-card__favorite_active' : 'movies-card__favorite_inactive'
 	}`;
+
+
+
 
 	const handleFavoriteButton = (event) => {
 		if (event.target.classList.contains('movies-card__favorite_active')) {
@@ -22,6 +24,11 @@ const MoviesCard = ({trailerLink, imageLink, duration, nameRU}) => {
 		}
 	};
 
+	const handleDeleteMovie = () => {
+		
+		
+	}
+
 	return (
 		<li className='movies-card__item'>
 			<a
@@ -30,14 +37,27 @@ const MoviesCard = ({trailerLink, imageLink, duration, nameRU}) => {
 				className='movies-card__trailer-link'
 				rel='noreferrer'
 			>
-				<img src={imageLink} alt='фото фильма' className='movies-card__image'/>
+				<img src={imageLink} alt='фото фильма' className='movies-card__image' />
 			</a>
 			<div className='movies-card__block'>
 				<h3 className='movies-card__title'>{nameRU}</h3>
-				<button
-					className={cardLikeButtonClassName}
-					onClick={handleFavoriteButton}
-				></button>
+
+				{location.pathname === '/movies' && (
+					<button
+						type='button'
+						className={cardLikeButtonClassName}
+						onClick={handleFavoriteButton}
+					></button>
+				)}
+				{
+					location.pathname === '/saved-movies' && (
+						<button
+						type='button'
+						className='movies-card__delete'
+						onClick={handleDeleteMovie}
+					></button>
+					)
+				}
 			</div>
 			<div className='movies-card__time'>
 				{hours}ч{minutes}м
